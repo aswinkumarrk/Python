17aa6618627521b82f14789455ed440c03b25356
aswinkumar <37615551+aswinkumarrk@users.noreply.github.com>
2018-04-14 15:15:01 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,4 @@
+print"Hello!! AswinKumar!!"
+print "Haha! Baby This is my first code in python,few of my python friends have said me python is a baby language,It is very easy to learn,just try it out."
+print" I am now here learning python"
+print "Are you joining me to learn python? you are always welcome!"
